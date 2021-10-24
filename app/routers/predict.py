5cc58b61a4f3290aa10b3b2b46ccd3209967cc1a
Khiem Le <theinnocentman1@gmail.com>
2021-10-24 15:38:05 +0700

    Config tokenizer

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -6,6 +6,21 @@ from ..vlp.pytorch_pretrained_bert.tokenization import BertTokenizer
 
 router = APIRouter()
 
+class Config():
+    bert_model = 'bert-base-multilingual-cased'
+    do_lower_case = False
+    max_tgt_length = 20
+    len_vis_input = 100
+
+args = Config()
+
+tokenizer = BertTokenizer.from_pretrained(
+    args.bert_model, do_lower_case=args.do_lower_case)
+
+args.max_seq_length = args.max_tgt_length + \
+    args.len_vis_input + 3  # +3 for 2x[SEP] and [CLS]
+tokenizer.max_len = args.max_seq_length
+
 @router.get('/predict')
 def predict():
 
