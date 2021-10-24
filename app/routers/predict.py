22273b741a4891f6a0400e5972b25d0c471cc68d
Khiem Le <theinnocentman1@gmail.com>
2021-10-24 18:03:57 +0700

    TEst

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -67,28 +67,30 @@ def predict():
                 w_list.append(w)
         forbid_ignore_set = set(tokenizer.convert_tokens_to_ids(w_list))
 
-    print('Load model...')
-    for model_recover_path in glob.glob(args.model_recover_path.strip()):
-        model_recover = torch.load(model_recover_path)
-        model = BertForSeq2SeqDecoder.from_pretrained(args.bert_model,
-                                                    max_position_embeddings=args.max_position_embeddings, config_path=args.config_path,
-                                                    state_dict=model_recover, num_labels=cls_num_labels,
-                                                    type_vocab_size=type_vocab_size, task_idx=3, mask_word_id=mask_word_id,
-                                                    search_beam_size=args.beam_size, length_penalty=args.length_penalty,
-                                                    eos_id=eos_word_ids, forbid_duplicate_ngrams=args.forbid_duplicate_ngrams,
-                                                    forbid_ignore_set=forbid_ignore_set, ngram_size=args.ngram_size, min_len=args.min_len,
-                                                    enable_butd=args.enable_butd, len_vis_input=args.len_vis_input)
-        del model_recover
-    print('Model loaded')
+    # print('Load model...')
+    # for model_recover_path in glob.glob(args.model_recover_path.strip()):
+    #     model_recover = torch.load(model_recover_path)
+    #     model = BertForSeq2SeqDecoder.from_pretrained(args.bert_model,
+    #                                                 max_position_embeddings=args.max_position_embeddings, config_path=args.config_path,
+    #                                                 state_dict=model_recover, num_labels=cls_num_labels,
+    #                                                 type_vocab_size=type_vocab_size, task_idx=3, mask_word_id=mask_word_id,
+    #                                                 search_beam_size=args.beam_size, length_penalty=args.length_penalty,
+    #                                                 eos_id=eos_word_ids, forbid_duplicate_ngrams=args.forbid_duplicate_ngrams,
+    #                                                 forbid_ignore_set=forbid_ignore_set, ngram_size=args.ngram_size, min_len=args.min_len,
+    #                                                 enable_butd=args.enable_butd, len_vis_input=args.len_vis_input)
+    #     del model_recover
+    # print('Model loaded')
 
-    print('Load model to GPU')
-    if args.fp16:
-        model.half()
-    model.to(device)
-    if n_gpu > 1:
-        model = torch.nn.DataParallel(model)
-    torch.cuda.empty_cache()
-    model.eval()
-    print('Model is now on GPU')
-    is_ready = next(model.parameters()).is_cuda
-    return {'is_ready': is_ready}
+    # print('Load model to GPU')
+    # if args.fp16:
+    #     model.half()
+    # model.to(device)
+    # if n_gpu > 1:
+    #     model = torch.nn.DataParallel(model)
+    # torch.cuda.empty_cache()
+    # model.eval()
+    # print('Model is now on GPU')
+    # is_ready = next(model.parameters()).is_cuda
+    # return {'is_ready': is_ready}
+
+    return {}
