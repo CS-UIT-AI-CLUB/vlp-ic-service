33e26435a9619805b885df141ed2a01d52696387
Khiem Le <theinnocentman1@gmail.com>
2021-10-24 14:20:53 +0700

    Bug

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join('..', 'vlp')))
 
 from fastapi import FastAPI
 from .routers import check_cuda, predict
-from vlp.vlp.pytorch_pretrained_bert.modeling import BertForSeq2SeqDecoder
-from vlp.vlp import seq2seq_loader
+from .vlp.vlp import seq2seq_loader
 
 app = FastAPI()
 
