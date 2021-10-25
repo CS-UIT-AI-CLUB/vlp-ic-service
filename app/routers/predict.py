867cf844f3e75224dcad5bdb3095dfab0b77ba9b
Khiem Le <theinnocentman1@gmail.com>
2021-10-25 12:55:15 +0700

    Test docker image file

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -90,7 +90,7 @@ if args.forbid_ignore_word:
             w_list.append(w)
     forbid_ignore_set = set(tokenizer.convert_tokens_to_ids(w_list))
 
-# print('Load model...')
+print('Load model...')
 for model_recover_path in glob.glob(args.model_recover_path.strip()):
     model_recover = torch.load(model_recover_path)
     model = BertForSeq2SeqDecoder.from_pretrained(args.bert_model,
