d58e5e24b898d3a2e619eac8c0e0537407db8f26
Khiem Le <theinnocentman1@gmail.com>
2021-10-25 13:09:25 +0700

    Fix network issue

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -136,7 +136,8 @@ def predict(file: UploadFile = File(...)):
     # region_bbox_vec = np.array(f_bbox[img_id])
 
     f = {'image': file.file.read()}
-    result = requests.post('http://0.0.0.0:5055/api/detectron_vlp', files=f)
+    result = requests.post(
+        'http://detectron-vlp-api:5055/api/detectron_vlp', files=f)
     try:
         result = result.json()
     except json.JSONDecodeError:
